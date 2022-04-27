4f2cf706c9cf7e58b11bc177f763f88c388b5555
tlemen <102385324+tlemen@users.noreply.github.com>
2022-04-27 10:40:57 -0700

    Create 04272022_messiGoals.js

---
+++ 8kyu/04272022_messiGoals.js
@@ -0,0 +1,17 @@
+//NAME : TIMOTHY LEMEN
+//DATE : 04272022
+//CODEWARS KATA LEVEL 8
+
+//KATA DESCRIPTION
+//================
+//Add up Messi's goals in three leagues
+
+
+//Code Goes below here
+//====================
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals+copaDelReyGoals+championsLeagueGoals
+  } 
+
+  //also possible to use the spread function
+
