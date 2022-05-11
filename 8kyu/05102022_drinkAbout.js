92f30000a5480673a1280363142d5bc8a854d6a9
tlemen <102385324+tlemen@users.noreply.github.com>
2022-05-10 19:46:04 -0700

    Create 05102022_drinkAbout.js

---
+++ 8kyu/05102022_drinkAbout.js
@@ -0,0 +1,31 @@
+//NAME : TIMOTHY LEMEN
+//DATE : 05102022
+//CODEWARS KATA LEVEL 5
+
+//KATA DESCRIPTION
+//================
+// Kids drink toddy.
+// Teens drink coke.
+// Young adults drink beer.
+// Adults drink whisky.
+// Make a function that receive age, and return what they drink.
+
+// Rules:
+
+// Children under 14 old.
+// Teens under 18 old.
+// Young under 21 old.
+// Adults have 21 or more.
+
+//Code Goes below here
+//====================
+function peopleWithAgeDrink(old) {
+    if(old>=21){
+      return 'drink whisky'
+    }else if(old>17){
+      return 'drink beer'
+    }else if(old>13){
+      return 'drink coke'
+    }else{
+      return 'drink toddy';
+  }};
