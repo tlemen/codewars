89e21ff4495ab703e2eebda0ca4c418f5dcdcab7
tlemen <102385324+tlemen@users.noreply.github.com>
2022-05-14 21:08:28 -0700

    Create 05142022_plural.js

---
+++ 8kyu/05142022_plural.js
@@ -0,0 +1,21 @@
+//NAME : TIMOTHY LEMEN
+//DATE : 05142022
+//CODEWARS KATA LEVEL 8
+
+//KATA DESCRIPTION
+//=================
+// We need a simple function that determines if a plural is needed or not. It should take a number, and return true if a plural should be used with that number or false if not. This would be useful when printing out a string such as 5 minutes, 14 apples, or 1 sun.
+
+// You only need to worry about english grammar rules for this kata, where anything that isn't singular (one of something), it is plural (not one of something).
+
+// All values will be positive integers or floats, or zero.
+
+//Code Goes below here
+//====================
+function plural(n) {
+    return n===1 ? false : true  
+  }
+
+//as an arrow function:
+
+const plural = n => n !== 1
