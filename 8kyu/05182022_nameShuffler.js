ef9ebcac355ef8b321f916721ed11042b0a42d17
tlemen <102385324+tlemen@users.noreply.github.com>
2022-05-18 21:12:49 -0700

    Create 05182022_nameShuffler.js

---
+++ 8kyu/05182022_nameShuffler.js
@@ -0,0 +1,16 @@
+//NAME : TIMOTHY LEMEN
+//DATE : 0518
+2022
+//CODEWARS KATA LEVEL 8
+
+//KATA DESCRIPTION
+//=================
+//Create a function that reverses the order of a given name.
+
+
+//Code Goes below here
+//====================
+function nameShuffler(str){
+  return str.split(' ').reverse().join(' ')
+}
+
