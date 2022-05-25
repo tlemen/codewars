3671828ddaa4028ae65e288cc0ab467f5813445c
tlemen <102385324+tlemen@users.noreply.github.com>
2022-05-25 10:40:40 -0700

    Create 05252022_replaceVowelsWithExlamationPoints.js

---
+++ 8kyu/05252022_replaceVowelsWithExlamationPoints.js
@@ -0,0 +1,13 @@
+//NAME : TIMOTHY LEMEN
+//DATE : 05252022
+//CODEWARS KATA LEVEL 8
+
+//KATA DESCRIPTION
+//=================
+//repace all vowels with exclamation points
+
+//Code Goes below here
+//====================
+function replace(s){
+    return s.replace(/[aeiou]/gi,'!')  
+  }
